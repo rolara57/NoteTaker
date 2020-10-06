4f51fd0f9ad26f0449a3237877963b148862c638
rolara57 <roger@qm46.com>
2020-10-06 11:52:53 -0500

    addind htmroute

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -3,14 +3,14 @@ const express = require("express");
 const fs = require("path");
 //set up Express app
 const app = express();
-var Port = 3000;
+const Port = 3000;
 //sets up the Express app to handle data parsing
-app.unsubscribe(express.urlencoded({ extend: true}));
+app.use(express.urlencoded({ extend: true}));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("Develop/public"));
 //Router 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 //Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+//dependencies 
+const path = require("path");
+//Routing
+module.exports = function(app) {
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
+      });
+    
+      
+    
+      // If no matching route is found default to home
+      app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+      });
+    };
